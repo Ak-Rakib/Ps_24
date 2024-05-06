081a711f5c8d7e4e0b4a4f419776ee6e00030578
Ak-Rakib <rkrakibsaimon@gmail.com>
2024-05-06 10:20:11 +0600

    using building function

--- Day_10/copy_string.c
+++ Day_10/copy_string.c
@@ -4,9 +4,16 @@ int main()
 {
     char a[100], b[100];
     scanf("%s %s",a,b);
-    for(int i = 0; i <= strlen(a); i++){
-        a[i] = b[i];
-    }
-    printf("%s %s", a,b);
+
+    // using building function for copying string "strcpy()"
+    strcpy(a,b);
+    printf("%s %s",a,b); 
+    
+
+    // Below this process logically
+    // for(int i = 0; i <= strlen(a); i++){
+    //     a[i] = b[i];
+    // }
+    // printf("%s %s", a,b);
     return 0;
 }
--- F_Way_Too_Long_Words.c
+++ F_Way_Too_Long_Words.c
@@ -4,6 +4,7 @@ int main()
 {
     char a[100];
     fgets(a,100,stdin);
+    strlen(a);
     printf("%s", a);
     return 0;
 }
