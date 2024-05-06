577d0fa4ad3880213589c0372348fb7ffc984dbe
Ak-Rakib <rkrakibsaimon@gmail.com>
2024-05-06 11:05:42 +0600

    use building function for lexicographical_compare

--- Day_10/lexicographical_compare.c
+++ Day_10/lexicographical_compare.c
@@ -4,40 +4,55 @@ int main()
 {
     char a[100], b[100];
     scanf("%s %s", a, b);
-    int i = 0;
-    while (1)
-    {
-        if (a[i] == '\0' && b[i] == '\0')
-        {
-            printf("A and B same\n");
-            break;
-        }
-        else if (a[i] > b[i])
-        {
-            printf("A is getter then B\n");
-            break;
-        }
-        else if (a[i] < b[i])
-        {
-            printf("B is getter then A\n");
-            break;
-        }
 
-        if (a[i] == b[i])
-        {
-            i++;
-        }
-        else if (a[i] > b[i])
-        {
-            printf("A is getter B\n");
-            break;
-        }
-        else
-        {
-            printf("B is getter then A\n");
-            break;
-        }
+    // Use building function for lexicographical compare "strcmp()"
+    int value = strcmp(a,b);
+    // printf("%d ",value);
+
+    if(value > 0){
+        printf("A is getter then B");
+    } else if(value == 0){
+        printf("A and B same");
+    } else {
+        printf("B is getter then A");
     }
+
+
+    // Below this code is logical
+    // int i = 0;
+    // while (1)
+    // {
+    //     if (a[i] == '\0' && b[i] == '\0')
+    //     {
+    //         printf("A and B same\n");
+    //         break;
+    //     }
+    //     else if (a[i] > b[i])
+    //     {
+    //         printf("A is getter then B\n");
+    //         break;
+    //     }
+    //     else if (a[i] < b[i])
+    //     {
+    //         printf("B is getter then A\n");
+    //         break;
+    //     }
+
+    //     if (a[i] == b[i])
+    //     {
+    //         i++;
+    //     }
+    //     else if (a[i] > b[i])
+    //     {
+    //         printf("A is getter B\n");
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         printf("B is getter then A\n");
+    //         break;
+    //     }
+    // }
 
     return 0;
 }
