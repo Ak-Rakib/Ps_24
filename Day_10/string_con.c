50098e7d119cef4736e3acbbf7358b4c1944cedc
Ak-Rakib <rkrakibsaimon@gmail.com>
2024-05-06 16:01:52 +0600

    use logical and building function solve those problem

---
+++ Day_10/string_con.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[200], b[100];
+    scanf("%s %s", a,b);
+    strcat(a,b);
+    printf("%s %s", a,b);
+
+    // below this logically
+    // int k = strlen(a);
+    // for(int j = 0; j <= strlen(b); j++){
+    //     a[k] = b[j];
+    //     k++;
+    // }
+    // printf("%s %s",a,b);
+    return 0;
+}
